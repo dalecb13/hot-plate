c2e1d5a3f31cad396bae7538ec24972152062c26
dalecb13 <dalecb13@gmail.com>
2025-03-15 14:28:28 +0100

    added initial constants

---
+++ constants/map.ts
@@ -0,0 +1,3 @@
+export const MAPTILER_API_KEY = process.env.EXPO_PUBLIC_MAPTILER_KEY!;
+export const MAPTILER_STYLE_URL =
+  `https://api.maptiler.com/maps/streets-v2/style.json?key=${MAPTILER_API_KEY}`;
---
+++ constants/onboarding.ts
@@ -0,0 +1,23 @@
+export const onboarding = [
+  {
+    id: 1,
+    title: "The perfect restaurant is just a tap away!",
+    description:
+      "Your journey begins with Restaurant Swipe. Find your ideal restaurant effortlessly.",
+    // image: images.onboarding1,
+  },
+  {
+    id: 2,
+    title: "Find your ideal meal with Restaurant Swipe",
+    description:
+      "Have fun finding the perfect restaurant with your friends.",
+    // image: images.onboarding2,
+  },
+  {
+    id: 3,
+    title: "Your restaurant, your way. Let's go!",
+    description:
+      "Pick your settings, look at your choices, and swipe away!",
+    // image: images.onboarding3,
+  },
+];
